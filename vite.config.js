81cf84ae056f6f119f82e42a1151dcdbd2a70612
William Moore <w.moore@dundee.ac.uk>
2022-05-27 09:47:27 +0100

    Try to fix deploy on netlify

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 // vite.config.js
-export default {
-  // config options
-  base: `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/ome-ngff-validator/`
-};
+let config = {};
+// this will be undefined when deployed from netlify, but is used by gh-pages
+if (process.env.GITHUB_REPOSITORY_OWNER) {
+  config.base = `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/ome-ngff-validator/`;
+}
+
+export default config;
