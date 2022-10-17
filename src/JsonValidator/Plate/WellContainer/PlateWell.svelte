ccd092a45e9b1e7b1a3eacef102335ff1aeb5618
William Moore <w.moore@dundee.ac.uk>
2022-10-17 15:00:34 +0100

    Wells support ?well=1 or ?well=all for validating all images

--- src/JsonValidator/Plate/WellContainer/PlateWell.svelte
+++ src/JsonValidator/Plate/WellContainer/PlateWell.svelte
@@ -1,15 +1,37 @@
 <script>
-  import { getJson, validate } from "../../../utils";
+  import { getJson, validate, getSearchParam, range } from "../../../utils";
 
   export let wellAttrs;
   export let source;
   export let path;
 
+  const wellToValidate = getSearchParam("well");
+  let wellIndex = parseInt(wellToValidate);
+  let wellIndices = [0];
+  if (isNaN(wellIndex)) {
+    if (wellToValidate == "all") {
+      wellIndices = range(wellAttrs.well.images.length);
+    }
+  } else {
+    wellIndices = [wellIndex];
+  }
+
+  let loadingIndex = 0;
+
   async function loadAndValidate() {
     console.log("wellAttrs", wellAttrs, wellAttrs.well);
-    let imgPath = wellAttrs.well.images[0].path;
-    let imgAttrs = await getJson(source + path + "/" + imgPath + "/.zattrs");
-    let errs = await validate(imgAttrs);
+    let errs = [];
+    const imgs = wellAttrs.well.images;
+    for (let i=0; i < wellIndices.length; i++) {
+      // this will fail if e.g. any getJson() raises exception
+      let index = wellIndices[i];
+      if (index >= imgs.length) {
+        return ["Invalid Well index: " + index];
+      }
+      loadingIndex = index;
+      let imgAttrs = await getJson(source + path + "/" + imgs[index].path + "/.zattrs");
+      errs = errs.concat(await validate(imgAttrs));
+    };
     return errs;
   }
 
@@ -26,9 +48,17 @@
   <td class={errors.length === 0 ? "valid" : "invalid"}>
     <a title="{path}: Open Well" href="{url}?source={source + path}/">
       {#await imagePromise}
-        &nbsp
+        {#if wellToValidate == "all"}
+          <span title="Loading Well index: {loadingIndex}" style="color:rgba(0,0,0,0.2)">{loadingIndex}</span>
+        {/if}
       {:then imgErrors}
-        {imgErrors.length === 0 ? "✓" : "⨯"}
+        {#if imgErrors.length === 0}
+          ✓
+        {:else}
+          <span title={imgErrors.join(", ")} style="color:red">⨯</span>
+        {/if}
+      {:catch error}
+        <span style="color:red">⨯</span>
       {/await}
     </a>
   </td>
